122fea1cbfb0031768d3ab201d56aa9f7e9e96f1
SugeidyMP <37090218+SugeidyMP@users.noreply.github.com>
2018-10-08 14:00:26 -0500

    suma de arreglos

---
+++ SumaArreglo.py
@@ -0,0 +1,11 @@
+__author__ = 'sugeidy mejia popoca'
+
+def SumaNumeroEntero(numero):
+    suma = 0
+    for i in numero:
+        suma = suma + i
+    return suma
+print(SumaNumeroEntero([3,5,8,10,15,25]))
+
+
+
